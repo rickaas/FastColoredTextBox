0cfbb81e3d333b36fa32f9823ad15ff2ec2ad72a
rickaas <saakcir@gmail.com>
2014-06-18 23:22:21 +0200

    Unit test attempt for InsertTextCommand

---
+++ UnitTests/Commands/InsertTextCommandTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FastColoredTextBoxNS;
+using FastColoredTextBoxNS.CommandImpl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Commands
+{
+    [TestClass]
+    public class InsertTextCommandTests
+    {
+        private string[] GetLines()
+        {
+            return new string[] {
+                "foo",
+                "bar",
+                "cheese",
+                "",
+                "last",
+            };
+        }
+
+        private string GetCRLFLine()
+        {
+            return String.Join("\r\n", GetLines());
+        }
+
+        [TestMethod]
+        public void InsertTextWithCRLF()
+        {
+            FastColoredTextBox fctb = new FastColoredTextBox();
+            string insertedText = GetCRLFLine();
+            var command = new InsertTextCommand(fctb.TextSource, insertedText);
+
+            Assert.AreEqual("", fctb.Text);
+            command.Execute();
+            Assert.AreEqual(GetCRLFLine(), fctb.Text);
+        }
+    }
+}
